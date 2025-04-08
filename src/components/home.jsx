ef6cfc48782faae875cddb4b3ad5240561c8a848
sandyyy741821 <santhoshsandyyy741821@gmail.com>
2025-04-08 16:21:42 +0530

    added netlify config

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -11,13 +11,13 @@ const LandingPage = () => {
         <h1 className="text-4xl font-bold mb-8">Welcome to Example.com</h1>
         <div className="flex flex-col space-y-6">
           <button
-            onClick={() => handleRedirect("https://sub.example.com")}
+            onClick={() => handleRedirect("https://sub-frontend-net.netlify.app/")}
             className="px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700"
           >
             Go to App
           </button>
           <button
-            onClick={() => handleRedirect("https://example.com:4321/blog")}
+            onClick={() => handleRedirect("https://astro-blog-net.netlify.app/")}
             className="px-6 py-3 bg-green-600 text-white rounded hover:bg-green-700"
           >
             Go to Blog
